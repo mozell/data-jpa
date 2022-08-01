0332f5f6f8c5b6910c4b219e845538cdb9f93fda
mozell <mozellatto@gmail.com>
2022-08-01 22:02:36 +0900

    쿼리 메소드 - 메소드 이름으로 쿼리 생성 - 2) 스프링 데이터 JPA

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -69,4 +69,33 @@ class MemberRepositoryTest {
         assertThat(deletedCount).isEqualTo(0);
     }
 
+    @Test
+    public void findByUsernameAndAgeGreaterThan() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //when
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        //then
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findTop3By() throws Exception {
+        //when
+        List<Member> top3 = memberRepository.findTop3By();
+    }
+
+    @Test
+    public void findDistinctBy() throws Exception {
+        //when
+        List<Member> top3 = memberRepository.findDistinctBy();
+    }
+
 }
