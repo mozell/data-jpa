0be966964c9f70601d6b36cf4109770b69a2212c
mozell <mozellatto@gmail.com>
2022-08-02 22:32:21 +0900

    쿼리 메소드 - @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -113,5 +113,20 @@ class MemberRepositoryTest {
         //then
         assertThat(findMember).isEqualTo(m1);
     }
+    
+    @Test
+    public void testQuery() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //when
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        //then
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
 
 }
